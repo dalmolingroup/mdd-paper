4da371e6b54d40e6a01490de824a2ee8acbc2b9b
jvfe <jvfecav@gmail.com>
2022-10-05 14:04:27 -0300

    Build table with all DGE genes

--- scripts/build_symreg_df.R
+++ scripts/build_symreg_df.R
@@ -6,34 +6,24 @@ load("results/txi/txi_gene.rda")
 
 load("results/important_variables/ann.rda")
 
-selected_genes <- c("ATAT1", "DDX39B", "ABR")
-
 genes_interest <- diff_df %>%
-  filter(hgnc_symbol == selected_genes) %>%
+  filter(type == "DGE") %>%
   select(gene, hgnc_symbol) %>%
   distinct()
 
 tpms <-
-  txi$abundance[rownames(txi$abundance) %in% genes_interest$gene,] %>%
+  txi$abundance[rownames(txi$abundance) %in% genes_interest$gene, ] %>%
   t() %>%
   as.data.frame() %>%
   tibble::rownames_to_column() %>%
-  setNames(., c("run", selected_genes))
+  setNames(., c("run", genes_interest$hgnc_symbol))
 
 full_data <- tpms %>%
   left_join(ann, by = "run") %>%
   mutate(# ??
     ph = ph[, 1],
     rin = rin[, 1]) %>%
-  mutate(
-    phenotype_reg = ifelse(phenotype == "MDD", 1, 0),
-    # gender_num = ifelse(gender == "female", 1, 0),
-    value = 1
-  ) %>%
-  # select(-c(group))
-  tidyr::spread(region, value, fill = 0) %>%
-  select(-c(group, `<NA>`))
+  mutate(phenotype_reg = ifelse(phenotype == "MDD", 1, 0),) %>%
+  select(-c(group))
 
-readr::write_tsv(full_data, "results/sym_reg/selected_genes_for_reg.tsv")
-
-
+readr::write_tsv(full_data, "results/sym_reg/genes_for_reg.tsv")
